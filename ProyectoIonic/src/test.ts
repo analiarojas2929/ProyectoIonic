71fa10ccf82c27f5ec12b9dfa4f77bfb9ea3b5e5
analiarojas2929 <analia.rojas@duocuc.cl>
2025-02-26 11:44:12 -0300

    commits

--- ProyectoIonic/src/test.ts
+++ ProyectoIonic/src/test.ts
@@ -14,7 +14,7 @@ declare const require: {
   };
 };
 
-// First, initialize the Angular testing environment.
+
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
